32b5b570103e73c448cb3194d731c2c713d0a6f1
abdurrehman2001 <aabdurrehman491@gmail.com>
2021-10-14 23:29:01 +0500

    POST Method in Rest API in react

--- src/App.js
+++ src/App.js
@@ -1084,6 +1084,50 @@ import Dynamic_Rounting from './MyPracticeComponent/Dynamic_Routing';
 // export default App;
 
 
+
+//       POST Method in Rest API
+
+//    Anil ka code hai ku k mjh smjh nh aya to iska code lgaya 
+
+
+// function App() {
+//   const [name,setName]=useState("");
+//   const [email,setEmail]=useState("");
+//   const [mobile,setMobile]=useState("");
+//  function saveData()
+//  {
+//    let data={name,email,mobile}
+//  // console.warn(data);
+//    fetch("http://localhost:4000/todo", {
+//      method: "POST",
+//      headers: {
+//        'Accept': 'application/json',
+//        'Content-Type': 'application/json',
+//      },
+//      body:JSON.stringify(data)
+//    }).then((resp)=>{
+//      // console.warn("resp",resp);;
+//      resp.json().then((result)=>{
+//        console.warn("result",result)
+//      })
+//    })
+//  }
+//    return (
+//      <div className="App">
+//        <h1>POST API Example </h1>  
+//        <input type="text" name="name" value={name} onChange={(e)=>{setName(e.target.value)}}  /> <br /> <br />
+//        <input type="text" name="email"  value={email} onChange={(e)=>{setEmail(e.target.value)}} /> <br /> <br />
+//        <input type="text" name="mobile"  value={mobile} onChange={(e)=>{setMobile(e.target.value)}}/> <br /> <br />
+//        <button type="button" onClick={saveData} >Save New User</button>
+//      </div>
+//    );
+//  }
+//  export default App;
+
+
+
+
+
 function App() {
   return(
     <div className="App">
